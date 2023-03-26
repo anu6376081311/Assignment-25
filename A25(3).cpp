8d329afd886420b991dfa45195a08d6578a56a1e
anumeena <111556950+anu6376081311@users.noreply.github.com>
2023-03-26 15:40:42 +0530

    Add files via upload

---
+++ A25(3).cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+class factorial
+{
+    private :
+    int n;
+    int fact;
+    public :
+       void setN(int x)
+       {
+           n=x;
+       }
+       int getN()
+       {
+           return n;
+       }
+      int getFactorial()
+      {
+      return fact;
+      }
+      void calculateFactorial()
+      {
+          int f=1,i;
+          if(n<=0)
+          {
+              fact=1;
+              return;
+          }
+          for(i=1;i<=n;i++)
+          {
+              f=f*i;
+          }
+          fact = f;
+      }
+};
+int main()
+{
+    factorial f;
+    f.setN(5);
+    f.calculateFactorial();
+    cout<<"factorial of "<<f.getN()<<" is "<<f.getFactorial();
+    return 0;
+}
